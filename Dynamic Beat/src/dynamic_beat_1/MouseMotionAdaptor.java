74e9a8b1d83f641697ab139ead8727dc16ebfcc4
HanJunSeoJB <hjs123@jbnu.ac.kr>
2023-03-12 21:18:35 +0900

    Rythme

---
+++ Dynamic Beat/src/dynamic_beat_1/MouseMotionAdaptor.java
@@ -0,0 +1,20 @@
+package dynamic_beat_1;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class MouseMotionAdaptor implements MouseMotionListener {
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
